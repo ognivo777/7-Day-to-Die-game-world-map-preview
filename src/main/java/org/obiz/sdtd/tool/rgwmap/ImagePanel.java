b2666116b212c91b536fa1001f753b0ae293b216
Vladimir Taran <VSTaran@alfabank.ru>
2019-07-22 20:21:54 +0300

    Fix map resize

--- src/main/java/org/obiz/sdtd/tool/rgwmap/ImagePanel.java
+++ src/main/java/org/obiz/sdtd/tool/rgwmap/ImagePanel.java
@@ -13,10 +13,6 @@ public class ImagePanel extends JPanel {
 
     public ImagePanel(BufferedImage image) {
         this.image = image;
-//        width = (int) Math.round(image.getWidth() * scale);
-//        height = (int) Math.round(image.getHeight() * scale);
-//        setPreferredSize(new Dimension(width, height));
-
         mouseListener = new MouseListener();
         addMouseListener(mouseListener);
         addMouseMotionListener(mouseListener);
@@ -30,7 +26,7 @@ public class ImagePanel extends JPanel {
         int height;
         width = (int) Math.round(image.getWidth() * scale);
         height = (int) Math.round(image.getHeight() * scale);
-        g.drawImage(image, mouseListener.x, mouseListener.y, width, height, this); // see javadoc for more info on the parameters
+        g.drawImage(image, mouseListener.x, mouseListener.y, width, height, this);
     }
 
     class MouseListener implements java.awt.event.MouseListener, MouseMotionListener, MouseWheelListener {
@@ -47,18 +43,12 @@ public class ImagePanel extends JPanel {
         public void mousePressed(MouseEvent e) {
             startX = e.getX() - x;
             startY = e.getY() - y;
-            System.out.println("started");
-            System.out.format("pos: %d:%d\n", e.getX(), e.getY());
-
             started = true;
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
             started = false;
-            System.out.println("unstarted");
-            System.out.format("pos: %d:%d\n", e.getX(), e.getY());
-
         }
 
         @Override
@@ -74,7 +64,6 @@ public class ImagePanel extends JPanel {
         @Override
         public void mouseDragged(MouseEvent e) {
             if (started) {
-//                System.out.format("drag: %d:%d\n", e.getX() - x, e.getY() - y);
                 x = e.getX() - startX;
                 y = e.getY() - startY;
                 SwingUtilities.invokeLater(() -> e.getComponent().repaint(20));
@@ -91,43 +80,14 @@ public class ImagePanel extends JPanel {
         public void mouseWheelMoved(MouseWheelEvent e) {
             int Px = e.getX();
             int Py = e.getY();
-            System.out.format("pos: %d:%d\n", Px, Py);
-            System.out.println(e.getScrollAmount());
-            System.out.println(e.getWheelRotation());
-            System.out.println(e.getPreciseWheelRotation());
-            System.out.println(e.getScrollType());
             double d = 1 + e.getWheelRotation() / 10f;
-            System.out.println("d = " + d);
-
-            double oldScale = ImagePanel.this.scale;
             ImagePanel.this.scale *= d;
-            double dd = oldScale/scale;
-            System.out.println("scale = " + scale);
-            System.out.println("d = " + d);
-            System.out.println("dd = " + dd);
-//            int k = 0;
-
-//            startX = Px - k;
-//            startY = Py - k;
-
-//            x =  Math.round(Math.round((startX*(dd-1))/(1-dd)));
-//            y =  Math.round(Math.round((startY*(dd-1))/(1-dd)));
-
-//            x =  Math.round(Math.round(startX));
-//            y =  Math.round(Math.round(startY));
-
-//            x =  Math.round(Math.round(dd*(startX+x)-startX));
-//            y =  Math.round(Math.round(dd*(startY+y)-startY));
-
             this.x =  Math.round(Math.round(
                     Px - (Px - this.x)*d
             ));
             this.y =  Math.round(Math.round(
                     Py - (Py - this.y)*d
             ));
-
-
-            System.out.println(this.x + " : " + this.y);
             SwingUtilities.invokeLater(() -> e.getComponent().repaint(20));
         }
     }

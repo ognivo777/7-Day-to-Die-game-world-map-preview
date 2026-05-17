9db88671149d0890e90c604b2f581031c7e933bf
Таран Владимир Сергеевич <ognivo777@gmail.com>
2026-05-18 02:43:14 +0300

    speed up bump mapping paints

--- src/main/java/org/obiz/sdtd/tool/rgwmap/BumpMappingUtils.java
+++ src/main/java/org/obiz/sdtd/tool/rgwmap/BumpMappingUtils.java
@@ -1,7 +1,7 @@
 package org.obiz.sdtd.tool.rgwmap;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 import java.awt.image.DataBufferUShort;
 
 public class BumpMappingUtils {
@@ -51,81 +51,64 @@ public class BumpMappingUtils {
         }
     }
 
-    public static void paint(BufferedImage source, int width, int height, float[] nvX, float[] nvY, float[] nvZ)
+    public static void paint(
+            BufferedImage source,
+            int width,
+            int height,
+            float[] nvX,
+            float[] nvY,
+            float[] nvZ)
     {
-        int x, y;
-        float intensity;
+        final DataBufferInt buffer =
+                (DataBufferInt) source.getRaster().getDataBuffer();
 
-        DataBuffer dataBuffer = source.getRaster().getDataBuffer();
+        final int[] pixels = buffer.getData();
 
-        int newR, newG, newB;
-        int oldR, oldG, oldB;
+        final int lightX = width / 3;
+        final int lightY = height * 6;
+        final float lightZ = width * 0.1f;
 
-        //TODO multithread
-        for (x = 0; x < width; x++)
-        {
-            for (y = 0; y < height; y++)
-            {
-                intensity = 0.6f + 20*BumpIntensity (width/3, height*6, x, y, nvX, nvY, nvZ, width);
+        for (int y = 0; y < height; y++) {
 
-                // brighten slightly (ambient)
-                //intensity += 0.18;
-                //if (intensity > 1.0) intensity = (float)1.0;
+            final int row = y * width;
 
-//                int sample = source.getRaster().getSample(x, y, 0)>> 8 & 0xff;
-                int i = ImageMath.xy2i(source, x, y);
-                int rgb = dataBuffer.getElem(i);
-//                int rgb = source.getRGB(x, y);//>> 8 & 0xff;
-                oldR = rgb >> 16 & 0xff;
-                oldG = rgb >> 8 & 0xff;
-                oldB = rgb & 0xff;
+            for (int x = 0; x < width; x++) {
 
-                newR = (int)(intensity * oldR);
-                newG = (int)(intensity * oldG);
-                newB = (int)(intensity * oldB);
-                if (newR>255) newR = 255;
-                if (newG>255) newG = 255;
-                if (newB>255) newB = 255;
+                final int idx = row + x;
 
-                if (newR<0) newR = 0;
-                if (newG<0) newG = 0;
-                if (newB<0) newB = 0;
+                // light vector
+                final float lx = lightX - x;
+                final float ly = lightY - y;
 
-//                Color c = new Color (newR, newG, newB);
-//                source.setRGB(x, y, c.getRGB());
-                dataBuffer.setElem(i, ImageMath.getPureIntFromRGB(newR, newG, newB));
+                // normalize
+                final float invLen = 1.0f /
+                        (float)Math.sqrt(lx * lx + ly * ly + lightZ * lightZ);
 
+                // dot product
+                final float intensity =
+                        0.6f + 20.0f * (
+                                nvX[idx] * lx * invLen +
+                                        nvY[idx] * ly * invLen +
+                                        nvZ[idx] * lightZ * invLen
+                        );
+
+                final int rgb = pixels[idx];
+
+                int r = (int)(((rgb >>> 16) & 0xFF) * intensity);
+                int g = (int)(((rgb >>> 8) & 0xFF) * intensity);
+                int b = (int)((rgb & 0xFF) * intensity);
+
+                // fast clamp
+                r = r < 0 ? 0 : Math.min(r, 255);
+                g = g < 0 ? 0 : Math.min(g, 255);
+                b = b < 0 ? 0 : Math.min(b, 255);
+
+                pixels[idx] =
+                        (r << 16) |
+                                (g << 8) |
+                                b;
             }
         }
-    }
-
-    private static float BumpIntensity (int lightX, int lightY, int pixelX, int pixelY, float[] nvX, float[] nvY, float[] nvZ, int width)
-    {
-        float Nx, Ny, Nz;
-
-        // get normal vector of map
-        Nx = nvX[pixelY * width + pixelX];
-        Ny = nvY[pixelY * width + pixelX];
-        Nz = nvZ[pixelY * width + pixelX];
-
-        // make vector from pixel to light
-        float lightvX = (float)(lightX - pixelX);
-        float lightvY = (float)(lightY - pixelY);
-        float lightvZ = (float)(width/10);
-
-        // normalize
-        float length = (float)Math.sqrt (lightvX*lightvX + lightvY*lightvY + lightvZ*lightvZ);
-
-        lightvX /= length;
-        lightvY /= length;
-        lightvZ /= length;
-
-        // take dot product
-        float intensity = (Nx * lightvX + Ny * lightvY + Nz * lightvZ);
-
-//        if (intensity < 0.0f) intensity = 0.0f;
-
-        return intensity;
     }
 
 }
